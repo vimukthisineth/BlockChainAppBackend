7ba8578323904713210220201f066d72c2d00e4b
Vimukthi Sineth <vimukthisineth@gmail.com>
2020-01-03 10:08:27 +0530

    Spring boot, repositories, database and file storage with Rest API implemented

---
+++ src/main/java/com/sliit/research/blockchainbasedapplication/controller/FarmerController.java
@@ -0,0 +1,35 @@
+package com.sliit.research.blockchainbasedapplication.controller;
+
+import com.sliit.research.blockchainbasedapplication.exception.ResourceNotFoundException;
+import com.sliit.research.blockchainbasedapplication.model.Farmer;
+import com.sliit.research.blockchainbasedapplication.repository.FarmerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class FarmerController {
+
+    @Autowired
+    FarmerRepository farmerRepository;
+
+    @GetMapping("/farmers")
+    public List<Farmer> getAllFarmers(){
+        return farmerRepository.findAll();
+    }
+
+    @GetMapping("/farmers/{id}")
+    public Farmer getFarmerById(@PathVariable(value = "id") Long farmerId){
+        return farmerRepository.findById(farmerId)
+                .orElseThrow(() -> new ResourceNotFoundException("Farmer", "id", farmerId));
+    }
+
+    @PostMapping("/farmers")
+    public Farmer createFarmer(@Valid @RequestBody Farmer farmer){
+        return farmerRepository.save(farmer);
+    }
+
+}
---
+++ src/main/java/com/sliit/research/blockchainbasedapplication/model/Product.java
@@ -0,0 +1,49 @@
+package com.sliit.research.blockchainbasedapplication.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private Double farmerPrice;
+
+    private Double retailPrice;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getFarmerPrice() {
+        return farmerPrice;
+    }
+
+    public void setFarmerPrice(Double farmerPrice) {
+        this.farmerPrice = farmerPrice;
+    }
+
+    public Double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(Double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+}
