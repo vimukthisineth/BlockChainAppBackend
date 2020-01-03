2c7e623bca1fa39b44a5bc8c085c7aba4058a2d6
Vimukthi Sineth <vimukthisineth@gmail.com>
2020-01-03 14:08:18 +0530

    Authentication rest calls

--- src/main/java/com/sliit/research/blockchainbasedapplication/controller/AuthenticationController.java
+++ src/main/java/com/sliit/research/blockchainbasedapplication/controller/AuthenticationController.java
@@ -1,10 +1,31 @@
 package com.sliit.research.blockchainbasedapplication.controller;
 
+import com.sliit.research.blockchainbasedapplication.model.User;
+import com.sliit.research.blockchainbasedapplication.service.AuthenticationService;
+import com.sliit.research.blockchainbasedapplication.utils.AuthResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public AuthResponse attemptLogin(HttpServletRequest request, @Valid @RequestBody User user){
+        return authenticationService.login(user);
+    }
+
+    @PostMapping("/signup")
+    public AuthResponse attemptSignup(HttpServletRequest request, @Valid @RequestBody User user){
+        return authenticationService.signUp(user);
+    }
 }
