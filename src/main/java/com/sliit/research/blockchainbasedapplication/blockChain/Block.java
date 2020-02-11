8631605558746fe78250f4091d98c0e20e4e7ea3
Vimukthi Sineth <vimukthisineth@gmail.com>
2020-02-11 17:55:50 +0530

    Blockchain mining

--- src/main/java/com/sliit/research/blockchainbasedapplication/blockChain/Block.java
+++ src/main/java/com/sliit/research/blockchainbasedapplication/blockChain/Block.java
@@ -8,6 +8,7 @@ public class Block {
     public String previousHash;
     private String data; //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970.
+    private int nonce;
 
     //Block Constructor.
     public Block(String data,String previousHash ) {
@@ -24,5 +25,14 @@ public class Block {
                         data
         );
         return calculatedhash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
--- src/main/java/com/sliit/research/blockchainbasedapplication/blockChain/BlockChain.java
+++ src/main/java/com/sliit/research/blockchainbasedapplication/blockChain/BlockChain.java
@@ -9,6 +9,8 @@ public class BlockChain {
         return ourInstance;
     }
 
+    public int difficulty = 5;
+
     private ArrayList<Block> blockChain = new ArrayList<Block>();
 
     private BlockChain(Block genesisBlock) {
@@ -25,5 +27,33 @@ public class BlockChain {
 
     public int getBlockChainSize(){
         return blockChain.size();
+    }
+
+    public Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockChain.size(); i++) {
+            currentBlock = blockChain.get(i);
+            previousBlock = blockChain.get(i-1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            //check if hash is solved
+            if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+        return true;
     }
 }
